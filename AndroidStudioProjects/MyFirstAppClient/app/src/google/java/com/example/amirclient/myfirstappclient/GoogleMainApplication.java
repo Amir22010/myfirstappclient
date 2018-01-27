76c3809dde6fb62ed92d744863ffaf2c00af40f1
Amir Khan <khan.amir504@gmail.com>
2018-01-27 19:28:00 +0530

    Modified GoogleMainApplication

--- AndroidStudioProjects/MyFirstAppClient/app/src/google/java/com/example/amirclient/myfirstappclient/GoogleMainApplication.java
+++ AndroidStudioProjects/MyFirstAppClient/app/src/google/java/com/example/amirclient/myfirstappclient/GoogleMainApplication.java
@@ -4,6 +4,7 @@ package com.example.amirclient.myfirstappclient;
  * Created by Amir on 26-01-2018.
  */
 
+import com.example.amir.myfirstappclient.MainApplication;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class GoogleMainApplication extends MainApplication {
